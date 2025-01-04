15c7f0b8cac47f2b478cb0dc1461579cb73960e3
diaamohamed013 <diaamohamed013@gmail.com>
2025-01-04 04:55:46 +0200

    create database tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function student() {
+        return $this->hasOne(Student::class);
+    }
+
+    public function teacher() {
+        return $this->hasOne(Teacher::class);
+    }
 }
