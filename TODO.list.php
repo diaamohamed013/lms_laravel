6ffcf490c5ee393388ecb1a562db7de09b7372b6
Ahmed Twab El Wahab <128147750+AhmedTwabElwahab@users.noreply.github.com>
2025-01-05 15:04:23 +0300

    make todo list for work

---
+++ TODO.list.php
@@ -0,0 +1,27 @@
+<?php
+
+//ENG:Diaa
+//0// TODO:: make data fake for all models.
+//1// TODO:: configuration users login and register after ENG:ahmed making views.
+// TODO:: make dashboard view for teacher Features[
+//2// 'grades(create,update,index,delete)',
+//3// 'curses(create,update,index,delete)',
+//4// 'sections and details(create,update,index,delete)' => on a view,
+//5// 'Display all students in a page' [exclude => create,delete,show],
+// TODO:: 'Dashboard' => [ all dashboard arabic language without english
+//6//      'Total number of students',
+//7//      'Total paid',
+//8//      'total discount',
+//9//      'Total number of courses',
+//10//     'last 5 users register'
+//] .
+// TODO:: complete all controllers create,update,view,delete.
+
+
+
+//ENG:Ahmed
+// TODO:: make login view.
+// TODO:: make register view.
+// TODO:: make Subscriptions table columns.
+// TODO:: make integration with payments.
+// TODO:: test all features eng:Diaa added it.
